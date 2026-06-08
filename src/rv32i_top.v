832dff766c685548f8681bcfb1a4b75b98ea611b
Ashish Sinha <ashishsinha12.gkp@gmail.com>
2026-06-08 12:44:58 +0530

    Update rv32i_top.v

--- src/rv32i_top.v
+++ src/rv32i_top.v
@@ -11,6 +11,7 @@ module rv32i_top(
     wire [6:0] funct7;
     
     wire [31:0] immediate;       // for immediate generator
+    wire [2:0] imm_sel;
     
     wire [31:0] alu_result;      // for ALU
     wire alu_zero;
@@ -19,7 +20,10 @@ module rv32i_top(
     wire [3:0] alu_op;
     
     wire [31:0] mem_read_data;   // for data memory
-    
+
+    wire [31:0] rs1_data; // for register file
+    wire [31:0] rs2_data;
+    wire [31:0] write_back_data;
     
     //control signals
     wire RegWrite,MemRead,MemWrite,MemToReg,ALUSrc,Branch,Jump;
@@ -35,5 +39,12 @@ module rv32i_top(
     //Decoder instantiation
     instruction_decoder decoder_block(.instruction(instruction),.opcode(opcode),.rs1(rs1_addr)
                                  ,.rs2(rs2_addr),.rd(rd_addr),.funct3(funct3),.funct7(funct7));
+
+    //Immediate generator instantion
+    imm_generator imm_gen_block(.instruction(instruction),.imm_sel(imm_sel),.immediate(immediate));
+
+    //Register file instantiation
+    register_file regfile_block(.clk(clk),.rst(rst),.regwrite(RegWrite),.rs1(rs1_addr),.rs2(rs2_addr),
+                                .rd(rd_addr),.wd(write_back_data),.rd1(rs1_data),.rd2(rs2_data));
     
 endmodule
