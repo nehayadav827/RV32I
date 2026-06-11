579faba81ca6ffedc7c62ab664d0037be6a19ccc
Vanshika Gupta <147723490+vanshikag1410@users.noreply.github.com>
2026-06-11 18:56:13 +0530

    Update control_unit_tb.sv

--- unit_tests/control_unit_tb.sv
+++ unit_tests/control_unit_tb.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/11/2026 06:16:41 PM
-// Design Name: 
-// Module Name: control_unit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module control_unit_tb;
     logic [6:0]opcode;
     logic RegWrite;
