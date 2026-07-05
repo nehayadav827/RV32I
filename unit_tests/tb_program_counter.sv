a5bf3b4839ad5420339249e5c60701e82760a630
Anshika@2205 <anshika.singh24b@iiitg.ac.in>
2026-07-05 11:32:29 +0530

    Change reset signal from rst to rst_n in testbench

--- unit_tests/tb_program_counter.sv
+++ unit_tests/tb_program_counter.sv
@@ -1,14 +1,14 @@
-//TEST BENCH
+//TEST BENCH 
 `timescale 1ns / 1ps
 module tb_program_counter;
  logic        clk;
- logic        rst;
+ logic        rst_n;    
  logic [31:0] next_pc;
  logic [31:0] pc;
 
  program_counter uut (
  .clk     (clk),
- .rst     (rst),
+ .rst_n   (rst_n),  
  .next_pc (next_pc),
  .pc      (pc)
     );
@@ -21,17 +21,17 @@ module tb_program_counter;
 
   initial 
    begin
-   $dumpfile("dump.vcd");
-   $dumpvars(0, tb_program_counter);
-    rst     = 1'b0;
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_program_counter);
+    rst_n   = 1'b1;     
     next_pc = 32'h0000_0000;
 
-   $monitor("Time = %0dt | rst = %b | next_pc = %h | pc = %h", $time, rst, next_pc, pc);
+    $monitor("Time = %0dt | rst_n = %b | next_pc = %h | pc = %h", $time, rst_n, next_pc, pc);
 
   #2;
-  rst = 1'b1;        
+  rst_n = 1'b0;        
   #10;
-  rst = 1'b0;        
+  rst_n = 1'b1;        
 
  @(posedge clk);
    next_pc = 32'h0000_0004;
@@ -42,13 +42,13 @@ module tb_program_counter;
  @(posedge clk);
     next_pc = 32'h0000_000C;
  #3;              
-  rst = 1'b1;        
+  rst_n = 1'b0;        
    #2;
-  rst = 1'b0;
+  rst_n = 1'b1;        
  @(posedge clk);
     next_pc = 32'hAFFF_0000;
  @(posedge clk);
-   #5;
+    #5;
  $finish;
     end
 endmodule
