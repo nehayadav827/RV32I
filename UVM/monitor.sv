3bb6b964517b16e45abd6fb865e1f4a2ab46dc68
Hiten Kumar <hitenkr302@gmail.com>
2026-07-12 15:03:31 +0530

    Update monitor.sv
    updated build and connect phase

--- UVM/monitor.sv
+++ UVM/monitor.sv
@@ -1,6 +1,7 @@
 class rv32i_monitor extends uvm_monitor;
   
   `uvm_component_utils(rv32i_monitor)
+  uvm_analysis_port #(rv32i_seq_item) item_collected_port;
   
   //standard constructor
   function new (string name = "rv32i_monitor",uvm_component parent);
@@ -8,6 +9,11 @@ class rv32i_monitor extends uvm_monitor;
     super.new(name,parent);
     `uvm_info("Monitor Class","Constructor",UVM_MEDIUM)
     
+  endfunction
+  
+  //build phase
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
   endfunction
   
 endclass
