9ebcaa277d90f2f862b9010a5e83e9ded55b4895
Hiten Kumar <hitenkr302@gmail.com>
2026-07-12 15:04:07 +0530

    Update scoreboard.sv
    updated build and connect phase

--- UVM/scoreboard.sv
+++ UVM/scoreboard.sv
@@ -1,6 +1,7 @@
 class rv32i_scoreboard extends uvm_scoreboard;
 
   `uvm_component_utils(rv32i_scoreboard)
+  uvm_analysis_imp #(rv32i_seq_item, rv32i_scoreboard) item_collected_export;
   
   //standard constructor
   function new(string name = "rv32i_scoreboard",uvm_component parent);
@@ -8,6 +9,11 @@ class rv32i_scoreboard extends uvm_scoreboard;
     super.new(name,parent);
     `uvm_info("Scoreboard Class","Constructor",UVM_MEDIUM)
     
+  endfunction
+  
+  //build phase
+  function void build_phase(uvm_phase phase)'
+    super.build_phase(phase);  
   endfunction
   
 endclass
